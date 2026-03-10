8dc2ac098503b761b30539c2fb86edbde9ac6efd
KnottsLauren <s3784281@students.fscj.edu>
2026-03-09 21:46:22 -0400

    Add missing cypress files

--- SeniorHelperFrontEnd/cypress.config.ts
+++ SeniorHelperFrontEnd/cypress.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    baseUrl: "http://localhost:4200",
+
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.ts",
+
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
