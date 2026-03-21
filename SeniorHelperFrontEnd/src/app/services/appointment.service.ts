163689bf4eb40f645e084589195373ecaa4a6a97
dewey mccrary <S3800528@students.fscj.edu>
2026-03-20 20:56:43 -0400

    HUGE Calendar update

--- SeniorHelperFrontEnd/src/app/services/appointment.service.ts
+++ SeniorHelperFrontEnd/src/app/services/appointment.service.ts
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
-export interface AppointmentDto {
-  id?: number;
-  title: string;
-  notes: string;
-  location: string;
-  start: string;
-  end: string;
-}
-
-@Injectable({
-  providedIn: 'root'
-})
-export class AppointmentService {
-
-  private baseUrl = 'http://localhost:8080/api/appointments';
-
-  constructor(private http: HttpClient) {}
-
-  createAppointment(seniorId: number, appointment: AppointmentDto): Observable<AppointmentDto> {
-    const params = new HttpParams().set('seniorId', seniorId);
-
-    return this.http.post<AppointmentDto>(
-      this.baseUrl,
-      appointment,
-      { params }
-    );
-  }
-}
-=======
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -88,4 +54,3 @@ export class AppointmentService {
     return this.http.get<{ id: number }>(`${this.usersApiUrl}/me`).pipe(map((user) => user.id));
   }
 }
->>>>>>> 6c0b53fb3be5e8c0fff6bd2c5faba7c5267bda29
