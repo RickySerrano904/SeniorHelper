511a41dfb4667e68110d5e8c33674cc5e1e26993
RickyRicardo787 <rickyserrano904@outlook.com>
2026-04-07 09:51:20 -0400

    fix browser refresh bug by forwarding non-api routes to index.html

--- SeniorHelper/src/main/java/edu/fscj/cop3024c/seniorhelper/config/MvcConfig.java
+++ SeniorHelper/src/main/java/edu/fscj/cop3024c/seniorhelper/config/MvcConfig.java
@@ -1,8 +1,8 @@
 package edu.fscj.cop3024c.seniorhelper.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +12,14 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/public/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Forward browser refreshes on Angular routes to index.html.
+        registry.addViewController("/{path:^(?!api$|v3$|swagger-ui$|actuator$|h2-console$|error$)[^.]*$}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{path:^(?!api$|v3$|swagger-ui$|actuator$|h2-console$|error$)[^.]*$}/**")
+                .setViewName("forward:/index.html");
     }
 }
